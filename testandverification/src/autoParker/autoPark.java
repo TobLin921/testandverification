30a86a03a0df8a1f1d18b1415f21a09e03fe3c72
TobLindell <tobias.lindell@hotmail.com>
2017-02-01 12:59:38 +0100

    added java folder structure

---
+++ testandverification/src/autoParker/autoPark.java
@@ -0,0 +1,14 @@
+package autoParker;
+
+public class autoPark {
+
+	private void moveForward(){
+	/*
+	 * This method moves the car 1 meter forward, queries the two infrared
+	 * sensors through the isEmpty method described below and returns a data structure
+	 * that contains the current position of the car,  and the situation
+	 * of the detected parking places up to now. The car cannot be moved forward
+	 * beyond the end of the street. 
+	 */
+	}
+}
---
+++ testandverification/src/autoParkerTest/autoParkTest.java
@@ -0,0 +1,5 @@
+package autoParkerTest;
+
+public class autoParkTest {
+
+}
