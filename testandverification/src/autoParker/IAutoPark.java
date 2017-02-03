206eaf6400a057f0f8b65a5ad0d67a590cc1ca5c
TobLin921 <tobias.lindell@hotmail.com>
2017-02-03 16:18:35 +0100

    added the test names to the comments in the IAutoPark interface

--- testandverification/src/autoParker/IAutoPark.java
+++ testandverification/src/autoParker/IAutoPark.java
@@ -33,10 +33,10 @@ public interface IAutoPark {
 	Post-Condition: A value is entered into the street array at the index matching the car’s position.
 	
 	Test-Cases:
-	Test that an integer value is inserted into the street array when similar or same sensor readings are prepared.
-	Test that an integer value is inserted into the street array when one sensor has very erratic values prepared.
-	Test that a -1 is inserted into the street array when both sensors have very erratic values prepared.
-	Test that the car does nothing when it is already parked.
+	testIsEmptyNormal() : Test that an integer value is inserted into the street array when similar or same sensor readings are prepared.
+	testIsEmptySensorOneOnly() : Test that an integer value is inserted into the street array when one sensor has very erratic values prepared.
+	testIsEmptyNoSensor() : Test that a -1 is inserted into the street array when both sensors have very erratic values prepared.
+	testIsEmptyParked() : Test that the car does nothing when it is already parked.
 	*/
 	
 	public int isEmpty(ISensor sensor1, ISensor sensor2);
@@ -52,10 +52,10 @@ public interface IAutoPark {
 	Post-Condition: Car is one position behind where it was before, and there is a value registered in the new position’s index on the street.
 	
 	Test-Cases:
-	Test that the car’s position is decremented on one moveBackward call.
-	Test that the car is not able to move past the beginning of the street.
-	Test that the value of the position’s index in the street is different from what it was before.
-	Test that the car does nothing when it is already parked.
+	testMoveBackward() : Test that the car’s position is decremented on one moveBackward call.
+	testBeginningofStreet() : Test that the car is not able to move past the beginning of the street.
+	testMoveBackwardSensorChange() : Test that the value of the position’s index in the street is different from what it was before.
+	testMoveBackwardParked() : Test that the car does nothing when it is already parked.
 	*/
 	
 	public PositionStatus moveBackward();
@@ -71,11 +71,11 @@ public interface IAutoPark {
 	Post-Condition: Car is either parked, or is on the final position on the street.
 	
 	Test-Cases:
-	Test that the car parks when there is an empty spot behind the car.
-	Test that the program does not crash when there are no parking spots on the street.
-	Test that the car stops and parks when it has found an empty spot.
-	Test that the program does not crash when the car is parked and attempts to run park().
-	Test that the car does not find a spot without crashing when positioned on 4 or under.
+	testParkWithSpot() : Test that the car parks when there is an empty spot behind the car.
+	testParkWithNoSpot() : Test that the program does not crash when there are no parking spots on the street.
+	testParkForwardSpot() : Test that the car stops and parks when it has found an empty spot.
+	testParkWhenParked() : Test that the program does not crash when the car is parked and attempts to run park().
+	testParkWhenUnderFour() : Test that the car does not find a spot without crashing when positioned on 4 or under.
 	*/
 	
 	public void park();
@@ -91,9 +91,9 @@ public interface IAutoPark {
 	Post-Condition: The car is no longer parked, and has the same position that it had when it parked.
 	
 	Test-Cases:
-	Test that the car is no longer parked after unPark() is ran.
-	Test that parking a car and unparking it again does not affect its position.
-	Test that the car does nothing if the car is not parked already.
+	testUnPark() : Test that the car is no longer parked after unPark() is ran.
+	testUnParkPersistantPos() : Test that parking a car and unparking it again does not affect its position.
+	testUnParkWhenNotParked() : Test that the car does nothing if the car is not parked already.
 	*/
 	
 	public void unPark();
@@ -108,7 +108,7 @@ public interface IAutoPark {
 	
 	Post-Condition: The position value is returned.
 	Test-Cases:
-	Test that the value which is returned by whereIs() matches the current position of the car.
+	testWhereIs() : Test that the value which is returned by whereIs() matches the current position of the car.
 	*/
 	
 	public PositionStatus whereIs();
