94055255caad141e750d073227a91bf4856bf8b7
gusrisfr <gusrisfr@student.gu.se>
2017-02-17 14:04:29 +0100

    magic test push

--- testandverification/src/autoParkerTest/AutoParkIntegrationTest.java
+++ testandverification/src/autoParkerTest/AutoParkIntegrationTest.java
@@ -40,7 +40,7 @@ public class AutoParkIntegrationTest {
     	try{
 	    	ISensor mockSensor = mock(ISensor.class);
 	    	Stack<Integer> dists = new Stack<Integer>();
-	    	for(int i=0;i<2420;i++){
+	    	for(int i=0;i<1420;i++){
 	    		dists.push(2);
 	    	}
 	    	for(int i=0;i<25;i++){
@@ -52,6 +52,7 @@ public class AutoParkIntegrationTest {
 	    	for(int i=0;i<10;i++){
 	    		dists.push(-1);
 	    	}
+
 
 	    	ISensor mockBrokenSensor = mock(ISensor.class);
 	    	Stack<Integer> distsBroken = new Stack<Integer>();
@@ -72,9 +73,12 @@ public class AutoParkIntegrationTest {
 	    	}
 
 	    	Answer<Integer> answer = new StackAnswer<Integer>(dists);
+			Answer<Integer> answer2 = new StackAnswer<Integer>(distsBroken);
 	    	when(mockSensor.record()).thenAnswer(answer);
-	    	
-	    	AutoParkImpl autoPark = new AutoParkImpl(mockSensor, mockBrokenSensor);
+			when(mockBrokenSensor.record()).thenAnswer(answer2);
+
+
+			AutoParkImpl autoPark = new AutoParkImpl(mockSensor, mockBrokenSensor);
 	    	AutoParkImpl spyAutoPark = spy(autoPark);
 	    	
 	    	spyAutoPark.park();
@@ -84,6 +88,60 @@ public class AutoParkIntegrationTest {
     	}
     	
     }
+
+    @Test
+	public void integrationTestTwo(){
+    	try{
+			ISensor mockSensor = mock(ISensor.class);
+			Stack<Integer> dists = new Stack<Integer>();
+			for(int i=0;i<2450;i++){
+				dists.push(2);
+			}
+
+
+			ISensor mockBrokenSensor = mock(ISensor.class);
+			Stack<Integer> distsBroken = new Stack<Integer>();
+
+			for(int i=0;i<2400;i++){
+				distsBroken.push(2);
+			}
+			for(int i=0;i<50;i++){
+				distsBroken.push(-1);
+			}
+
+			Answer<Integer> answer = new StackAnswer<Integer>(dists);
+			Answer<Integer> answer2 = new StackAnswer<Integer>(distsBroken);
+			when(mockSensor.record()).thenAnswer(answer);
+			when(mockBrokenSensor.record()).thenAnswer(answer2);
+
+
+			AutoParkImpl autoPark = new AutoParkImpl(10, true, mockSensor, mockBrokenSensor);
+			AutoParkImpl spyAutoPark = spy(autoPark);
+
+			autoPark.setStreetValue(10, 6);
+			autoPark.setStreetValue(9, 6);
+			autoPark.setStreetValue(8, 6);
+			autoPark.setStreetValue(7, 6);
+			autoPark.setStreetValue(6, 6);
+
+			spyAutoPark.park();
+			spyAutoPark.park();
+			spyAutoPark.moveForward();
+			spyAutoPark.moveBackward();
+			spyAutoPark.whereIs();
+			spyAutoPark.unPark();
+			spyAutoPark.moveForward();
+			spyAutoPark.park();
+
+			verify(spyAutoPark, times(490)).moveForward();
+
+		}
+		catch (Exception e){
+    		fail("There should be no exceptions in this test");
+		}
+
+	}
+
 
 	
 }
